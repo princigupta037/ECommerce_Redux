8240ef6102ecccdaccbe0285b6eaf98dcbd687bb
princigupta037 <princygupta037@gmail.com>
2021-04-17 16:12:34 +0530

    Image Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-
-
-function App() {
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from '../src/components/Header'
+import Footer from '../src/components/Footer'
+import HomeScreen from './screens/HomeScreen'
+const App = () => {
   return (
-    <div className="App">
-      Welcome
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import products from '../products'
+import Product from '../components/Products'
+import { Row ,Col } from 'react-bootstrap'
+const HomeScreen = () => {
+    return (
+        <div>
+            <h1> Latest Products</h1>
+            <Row>
+                { products.map( product => (
+                    <Col sm={12} md={6} lg={4} xl={3}>
+                        <Product  product={product}/>
+                        {/* <h3> {product.name}</h3> */}
+                    </Col>
+                    
+                ))}
+            </Row>
+        </div>
+    )
+}
+
+export default HomeScreen
