d9e0ba3992635a3d49255cc078110df5f62ba406
princigupta037 <princygupta037@gmail.com>
2021-04-24 23:48:00 +0530

    Error Handler Update

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,30 @@
 import express from 'express'
-import products from './data/products.js'
 import connectDB from './config/db.js'
 import dotenv from "dotenv"
 import colors from 'colors'
+import productRoutes from './routes/productRoutes.js'
+import { notFound , errorHandler }  from './middleware/errorMiddleware.js'
 
-const app = express()
+
 dotenv.config()
 
 connectDB()
+const app = express()
+
+// app.use((req,res,next) => {
+//    //console.log('HELLO')
+//   //  console.log(req.originalUrl )
+//     next()
+// })
+
 app.get('/', (req,res) => {
     res.send('API is running...')
 })
+app.use('/api/products', productRoutes)
 
-app.get('/api/products', (req,res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req,res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
